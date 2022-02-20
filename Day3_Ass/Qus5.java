1ea3461d1a24030a22315783a1fac5a3166de11e
KESHAV TYAGI <60292531+iamkeshavtyagi@users.noreply.github.com>
2022-02-21 01:07:19 +0530

    Update Qus5.java

--- Day3_Ass/Qus5.java
+++ Day3_Ass/Qus5.java
@@ -1,10 +1,7 @@
 package Day3_Ass;
 
-public class Qus5 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public interface TimeClient {
+    public void setTime(int hour, int minute, int second);
+    public void setDate(int day, int month, int year);
+    public void setDateAndTime(int day, int month, int year,int hour, int minute, int second);
 }
